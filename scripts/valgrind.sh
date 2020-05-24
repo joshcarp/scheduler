94be0aba7b70a5029f4b74da1e2df7a7038544f2
joshcarp <joshuacarpeggiani@gmail.com>
2020-05-24 16:18:35 +1000

    Remove silly stuff

--- scripts/valgrind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for T in bin/*; do 
-		echo valgrind --leak-check=full $T
-		valgrind --demangle=yes --log-file=memcheck.log --leak-check=full $T 
-		grep "All heap blocks were freed" memcheck.log
-		rm memcheck.log
-	done
