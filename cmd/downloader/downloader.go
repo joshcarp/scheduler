abe47a33f1471123a9c53a60522c67e6db82cfad
joshcarp <joshuacarpeggiani@gmail.com>
2020-05-24 12:27:39 +1000

    Add gitlab ci

--- cmd/downloader/downloader.go
+++ cmd/downloader/downloader.go
@@ -6,7 +6,7 @@ import (
     "net/http"
     "os"
 )
-
+var filenum int
 func main() {
 	http.HandleFunc("/", uploadFile)
 	http.ListenAndServe(":80", nil)
@@ -15,7 +15,8 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
     fmt.Println(r)
 	res, _ := ioutil.ReadAll(r.Body)
-	ioutil.WriteFile("out.zip", res, os.ModePerm)
+	ioutil.WriteFile("out.tar", res, os.ModePerm)
+    filenum++
 
 
 
